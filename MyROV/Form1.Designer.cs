afbd4952d795563a26d1eebccd9cc9bf3e85fced
phapdn <phapdang7@gmail.com>
2023-05-09 10:40:30 +0700

    Add project files.

---
+++ MyROV/Form1.Designer.cs
@@ -0,0 +1,378 @@
+﻿namespace MyROV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbxCom = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.txbY1 = new System.Windows.Forms.TextBox();
+            this.txbX2 = new System.Windows.Forms.TextBox();
+            this.txbY2 = new System.Windows.Forms.TextBox();
+            this.txbX1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MintCream;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.lbStatus);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.cbxCom);
+            this.panel1.Controls.Add(this.btnConnect);
+            this.panel1.Location = new System.Drawing.Point(623, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(564, 134);
+            this.panel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(54, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 23);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Status:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.ForeColor = System.Drawing.Color.Red;
+            this.lbStatus.Location = new System.Drawing.Point(178, 94);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(96, 16);
+            this.lbStatus.TabIndex = 7;
+            this.lbStatus.Text = "Disconnected !";
+            this.lbStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "COM:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cbxCom
+            // 
+            this.cbxCom.FormattingEnabled = true;
+            this.cbxCom.Location = new System.Drawing.Point(181, 39);
+            this.cbxCom.Name = "cbxCom";
+            this.cbxCom.Size = new System.Drawing.Size(121, 24);
+            this.cbxCom.TabIndex = 2;
+            this.cbxCom.DropDown += new System.EventHandler(this.cbxCom_DropDown);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnConnect.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.ForeColor = System.Drawing.Color.Lime;
+            this.btnConnect.Location = new System.Drawing.Point(387, 59);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(110, 33);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connection";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.AccessibleName = "";
+            this.panel3.BackColor = System.Drawing.Color.MintCream;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel3.Controls.Add(this.btnStop);
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Controls.Add(this.button4);
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.button6);
+            this.panel3.Controls.Add(this.button3);
+            this.panel3.Controls.Add(this.button5);
+            this.panel3.Location = new System.Drawing.Point(623, 344);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(564, 192);
+            this.panel3.TabIndex = 4;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Image = global::MyROV.Properties.Resources.stop;
+            this.btnStop.Location = new System.Drawing.Point(301, 65);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 75);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::MyROV.Properties.Resources.bottom;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Location = new System.Drawing.Point(455, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 52);
+            this.button2.TabIndex = 4;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = global::MyROV.Properties.Resources.top;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button4.Location = new System.Drawing.Point(83, 16);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 52);
+            this.button4.TabIndex = 1;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::MyROV.Properties.Resources.top;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Location = new System.Drawing.Point(455, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 52);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImage = global::MyROV.Properties.Resources.right;
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button6.Location = new System.Drawing.Point(164, 76);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 52);
+            this.button6.TabIndex = 3;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::MyROV.Properties.Resources.left;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button3.Location = new System.Drawing.Point(3, 76);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 52);
+            this.button3.TabIndex = 0;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImage = global::MyROV.Properties.Resources.bottom;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button5.Location = new System.Drawing.Point(83, 133);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 52);
+            this.button5.TabIndex = 2;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.MintCream;
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel4.Location = new System.Drawing.Point(3, 4);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(614, 532);
+            this.panel4.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.MintCream;
+            this.panel6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel6.Controls.Add(this.txbY1);
+            this.panel6.Controls.Add(this.txbX2);
+            this.panel6.Controls.Add(this.txbY2);
+            this.panel6.Controls.Add(this.txbX1);
+            this.panel6.Controls.Add(this.label7);
+            this.panel6.Controls.Add(this.label6);
+            this.panel6.Controls.Add(this.label5);
+            this.panel6.Controls.Add(this.label4);
+            this.panel6.Location = new System.Drawing.Point(623, 144);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(564, 194);
+            this.panel6.TabIndex = 7;
+            // 
+            // txbY1
+            // 
+            this.txbY1.Location = new System.Drawing.Point(144, 122);
+            this.txbY1.Name = "txbY1";
+            this.txbY1.Size = new System.Drawing.Size(100, 22);
+            this.txbY1.TabIndex = 7;
+            // 
+            // txbX2
+            // 
+            this.txbX2.Location = new System.Drawing.Point(419, 33);
+            this.txbX2.Name = "txbX2";
+            this.txbX2.Size = new System.Drawing.Size(100, 22);
+            this.txbX2.TabIndex = 6;
+            // 
+            // txbY2
+            // 
+            this.txbY2.Location = new System.Drawing.Point(420, 122);
+            this.txbY2.Name = "txbY2";
+            this.txbY2.Size = new System.Drawing.Size(100, 22);
+            this.txbY2.TabIndex = 5;
+            // 
+            // txbX1
+            // 
+            this.txbX1.Location = new System.Drawing.Point(144, 33);
+            this.txbX1.Name = "txbX1";
+            this.txbX1.Size = new System.Drawing.Size(100, 22);
+            this.txbX1.TabIndex = 4;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(268, 121);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(134, 29);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "JoyRightY:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(267, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(135, 29);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "JoyRightX:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(18, 121);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 29);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "JoyLeftY:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(17, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 29);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "JoyLeftX:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(1192, 540);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ROV";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbxCom;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel3;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox txbY1;
+        private System.Windows.Forms.TextBox txbX2;
+        private System.Windows.Forms.TextBox txbY2;
+        private System.Windows.Forms.TextBox txbX1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MyROV/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using System.Xml;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace MyROV
+{
+    public partial class Form1 : Form
+    {
+        string dataRead = "";
+
+        delegate void SetTextCallback(string text); // Khai bao delegate SetTextCallBack voi tham so string
+        public Form1()
+        {   
+            InitializeComponent();
+        
+
+        }
+
+    
+        private void cbxCom_DropDown(object sender, EventArgs e)
+        {
+            if (SerialPort.GetPortNames().Length > 0)
+            {
+                cbxCom.DataSource = SerialPort.GetPortNames();
+                btnConnect.Enabled = true;
+            }
+            else
+            {
+                btnConnect.Enabled = false;
+            }
+        }
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!serialPort1.IsOpen)
+                {
+                    serialPort1.PortName = cbxCom.Text;
+                    serialPort1.BaudRate = 9600;
+                    serialPort1.Open();
+                    MessageBox.Show("Connection is successful!");
+                }
+                else 
+                {
+                    serialPort1.Close();
+                    MessageBox.Show("Disconnect is successful!");
+                    // lbStatus.Text = "Connected !";
+                    // lbStatus.ForeColor = Color.Green;
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                    MessageBox.Show("Connection is failed!\n"+ex.Message);
+
+            }
+
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+      
+            //lbStatus.Text = "Disconnected !";
+            //lbStatus.ForeColor = Color.Red;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                btnConnect.Text = "Disconnect";
+                btnConnect.ForeColor = Color.Red;
+
+                lbStatus.Text = "Connected !";
+                lbStatus.ForeColor = Color.Green;
+        
+               // btnConnect.Enabled = true;
+               // btnDisconnect.Enabled = false;
+            }
+            else
+            {
+                btnConnect.Text = "Connection";
+                btnConnect.ForeColor = Color.Green;
+
+                lbStatus.Text = "Disconnected !";
+                lbStatus.ForeColor = Color.Red;
+                //btnConnect.Enabled = false;
+                //btnDisconnect.Enabled = true;
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                dataRead = serialPort1.ReadLine().ToString().Trim();
+                TachChuoi(dataRead);
+            }catch
+            (Exception ex)
+            {
+                MessageBox.Show("Error");
+                throw;
+            }
+        }
+
+        private void TachChuoi(string dataRead)
+        {
+            if (this.txbX1.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(TachChuoi);
+                this.BeginInvoke(d, new object[] { dataRead});
+
+            }
+            else
+            {
+                Console.WriteLine("dataRead: " + dataRead);
+                string keyX1 = "X1:";
+                string keyY1 = "~Y1:";
+                string keyX2 = "X2:";
+                string keyY2 = "~Y2:";
+                TachJoystick(dataRead, keyX1, keyY1, txbX1, txbY1);
+                TachJoystick(dataRead, keyX2, keyY2, txbX2, txbY2);
+
+            }
+        }
+
+        private void TachJoystick(string data, string keyX, string keyY, System.Windows.Forms.TextBox txbX, System.Windows.Forms.TextBox txbY)
+        {
+            string value1, value2;
+            double value1XX, value2XX;
+            if (data.Contains(keyX) && data.Contains(keyY))
+            {
+                value1 = returnText(data, data.IndexOf(keyX) + keyX.Length, data.IndexOf(keyY) - keyY.Length + 1);
+                //value1 = dataRead.Substring(dataRead.IndexOf(key1)+key1.Length, dataRead.IndexOf(key2)-key2.Length+1);
+                double.TryParse(value1, out value1XX);
+                txbX.Text = value1XX.ToString();
+                if (value1XX >= 0)
+                {
+                    txbX.BackColor = Color.FromArgb(255, 255 - Convert.ToInt32(value1XX), 255 - Convert.ToInt32(value1XX));
+
+                }
+                else
+                {
+                    txbX.BackColor = Color.FromArgb(255 + Convert.ToInt32(value1XX), 255, 255 + Convert.ToInt32(value1XX));
+
+                }
+                value2 = returnText(data, data.IndexOf(keyY) + keyY.Length);
+                //value2 = dataRead.Substring(dataRead.IndexOf(key2)+key2.Length);
+                double.TryParse(value2, out value2XX);
+                txbY.Text = value2XX.ToString();
+                Console.WriteLine(value2.ToString());
+
+                if (value2XX >= 0)
+                {
+                    txbY.BackColor = Color.FromArgb(255, 255 - Convert.ToInt32(value2XX), 255 - Convert.ToInt32(value2XX));
+
+                }
+                else
+                {
+                    txbY.BackColor = Color.FromArgb(255 + Convert.ToInt32(value2XX), 255, 255 + Convert.ToInt32(value2XX));
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("None");
+            }
+        }
+        String returnText(string data, int pos1, int pos2)
+        {
+            return data.Substring(pos1, pos2);
+        }
+        String returnText(string data, int pos1)
+        {
+            return data.Substring(pos1);
+        }
+    }
+}
